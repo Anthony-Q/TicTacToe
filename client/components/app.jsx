15f7b2970d2262f4ae244224ff856fbf43482ae1
Anthony <Anthony@computers-MacBook-Pro.local>
2018-12-20 19:29:34 -0800

    Board created.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import TTTstyling from './style';
+
 
 class App extends React.Component {
     constructor(props) {
@@ -11,7 +13,23 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>Tic Tac Toe</div>
+            <TTTstyling>
+
+            <div className="board-container">
+              <h1>Tic Tac Toe -- React</h1>
+              <div className="board">
+                <div className="box">x</div>
+                <div className="box">x</div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+              </div>
+            </div>
+            </TTTstyling>
         )
     }
 }
---
+++ client/components/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const TTTstyling = styled.div`
+.board-container {
+    text-align: center;
+    align-content: center;
+}
+
+.box {
+    width: 100px;
+    height: 100px;
+    border: 1px solid black;
+    display: inline-block;
+}
+
+.board {
+    display: inline-flex;
+    flex-wrap: wrap;
+    width: 320px;
+
+}
+`
+
+export default TTTstyling;
